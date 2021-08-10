658d7ed30dcbd08405df0c09b3d8f2f8cc0eb998
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-08-10 13:35:39 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,32 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"атлас",
+        options:["atlas", "notepad", "pencil", "eraser"],
+        answer: 0
+    },
+    {
+        q:"ластик",
+        options:["pencil", "eraser", "notebook", "notepad"],
+        answer: 1
+    },
+    {
+        q:"блокнот",
+        options:["eraser", "pencil", "notepad", "atlas"],
+        answer: 2
+    },
+    {
+        q:"записная книжка",
+        options:["atlas", "pencil", "eraser", "notebook"],
+        answer: 3
+    },
+    {
+        q:"карандаш",
+        options:["pencil", "eraser", "notepad", "atlas"],
+        answer: 0
+    }
+    
+
+
+]
